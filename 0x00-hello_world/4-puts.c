de7a7b00828bd9c6de6165e1ec7e79e75cfc7971
dannykay62 <danielkay62@gmail.com>
2022-09-08 22:03:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - entry
  *
- *Return - Always 0 (Successful)
+ *Return: - Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
