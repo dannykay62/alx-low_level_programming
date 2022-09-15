3296a25919c12483575c7459e24daf60887e188a
dannykay62 <danielkay62@gmail.com>
2022-09-14 21:31:34 -0700

    Functions and nested if - C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  * main - print putchar
  *
  * Return: always 0
@@ -21,6 +20,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');									
+	_putchar('\n');								
+
 	return (0);
 }
