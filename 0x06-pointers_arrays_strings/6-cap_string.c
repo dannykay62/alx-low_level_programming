39c3613b10d64e104360cc571907141dac24dee2
dannykay62 <danielkay62@gmail.com>
2022-09-21 15:10:48 -0700

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int check_seperator(char c);
+int check_separator(char c);
 
 /**
  * cap_string - function capitalize all words of the string
@@ -18,10 +18,31 @@ char *cap_string(char *s)
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
 
-		if (check_seperator(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		if (check_separator(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
 	}
 
 	return (s);
+}
+
+/**
+ * check_separator - checks for word separators - space, new-line, tabs,
+ * ,, ;, ., !, ?, \", (, ), {, and }
+ * @c: input character
+ * Return: 1 if there is separator, 0 if not
+ */
+
+int check_separator(char c)
+{
+	int i = 0;
+	char separators[13] = {' ', '\t', '\n', ',', ';',
+		'.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (; i < 13; i++)
+	{
+		if (c == separator[i])
+			return (1);
+	}
+	return (0);
 }
