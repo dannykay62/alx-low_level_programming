107986ba814b6fe94b59edf2da351fa41fb8d546
dannykay62 <danielkay62@gmail.com>
2022-09-21 09:47:03 -0700

    Function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - function copy strings
+ * @dest: input string
+ * @src: input string
+ * @n: input integer
+ * Return: pointer to the result string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0;
+	int i = 0;
+	char *temp = dest;
+	char *start = src;
+
+
