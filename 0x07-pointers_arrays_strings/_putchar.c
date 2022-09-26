c84b10e7959362caa219ac8f1b3d3411f36c9543
dannykay62 <danielkay62@gmail.com>
2022-09-26 16:29:17 -0700

    function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,10 +1,13 @@
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: character to print
+ * Return: 1 for success
+ * error, return -1 with errno set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
