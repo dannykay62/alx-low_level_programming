91426c2e97b9fec7e6e2001c8122bb488dde42bb
dannykay62 <danielkay62@gmail.com>
2022-09-28 17:32:20 -0700

    function that prints a string in reverse recursively

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "daniel.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse using recursion
+ * @s: input string
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s){
+		_print_rev_recursion(s +1);
+		_putchar(*s);
+	}
+}
