8b7875f101135db9b5ce3bf38387949f6d11131e
dannykay62 <danielkay62@gmail.com>
2022-11-03 02:32:16 -0700

    function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+## Learning File descriptors
+
+## C Programming in Linux Tutorial #024 - open() read() write() Functions
+
+## C is Fun!!!
+
+## Doing Hard Things
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * FILE_ main.h
+ * AUTH: Daniel Adejoro
+ * Desc: Header file for project 0x15. C - File I/O
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
